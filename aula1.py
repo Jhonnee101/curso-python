a0f17f4fb89aed66448ffff136e4f526a83e9734
Joao Breno Gomes <138957799+Jhonnee101@users.noreply.github.com>
2024-07-24 22:36:01 -0300

    att

---
+++ aula1.py
@@ -0,0 +1,2 @@
+print("Atenção")
+print("Opa, editei")
---
+++ aula65.py
@@ -0,0 +1,9 @@
+#def Print ():
+#    print("Python é uma linguagem de programação de alto nível, interpretada, de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte. Foi lançada por Guido van Rossum em 1991.")
+
+def imprimir(a, b, c):    
+    print("Atenção")
+    print("Atenção")
+    print("Atenção")
+
+imprimir(1, 2, 3)
